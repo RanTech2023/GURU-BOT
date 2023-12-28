6178d58a700620d0c66f9e2f5c6e10cc2379d63a
RanTech2023 <138270707+RanTech2023@users.noreply.github.com>
2023-12-28 15:01:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['94705281879', 'Ransara', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
